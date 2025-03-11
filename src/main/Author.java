7effa6a1da9672d10aac92c994f3662cb0df6983
Your Name <youremail@example.com>
2025-03-11 10:20:03 +0000

    Author

---
+++ src/main/Author.java
@@ -0,0 +1,13 @@
+public class Author {
+
+    private String name;
+
+    private String getName(){
+        return name;
+    }
+
+    private void setName(String name){
+        this.name = name;
+    }
+    
+}
