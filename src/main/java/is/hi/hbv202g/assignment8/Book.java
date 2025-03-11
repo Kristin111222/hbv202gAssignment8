5ef0c976b0afd6850c54901ef99c4532bd3b318a
Your Name <youremail@example.com>
2025-03-11 10:56:15 +0000

    Book

---
+++ src/main/java/is/hi/hbv202g/assignment8/Book.java
@@ -0,0 +1,41 @@
+package is.hi.hbv202g.assignment8;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Book {
+    
+    private String title;
+
+    private List<Author> authors;
+
+    public Book(String title, String authorName) {
+        this.title = title;
+        this.authors = new ArrayList<Author>();
+    }
+
+    public Book(String title, List<Author> authors) {
+        this.title = title;
+        this.authors = authors;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(List<Author> authors) {
+        this.authors = authors;
+    }
+
+    public void addAuthor(Author author) {
+        this.authors.add(author);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
