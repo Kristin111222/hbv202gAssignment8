0e40418f4594fc7eaa7d9415257491ece0992aee
Gudrunmaria <gudrunmaria@gmail.com>
2025-03-11 10:59:13 +0000

    bætti við librarySystem

--- src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
+++ src/main/java/is/hi/hbv202g/assignment8/LibrarySystem.java
@@ -1,3 +1,55 @@
 package is.hi.hbv202g.assignment8;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibrarySystem {
+
+    private List<Lending> lendings;
+    private List<User> users;
+    private List<Book> books;
+    
+    public LibrarySystem(){
+        lendings = new ArrayList<Lending>();
+        users = new ArrayList<User>();
+        books = new ArrayList<Book>();
+    }
+
+    public void addBookWithTitleAndNameOfSingleAuthor(String title, String authorName){
+        books.add(new Book(title, authorName));
+    }
+
+    public void addBookWithTitleAndAuthorList(String title, List<Author> authors){
+        books.add(new Book(title, authors));
+    }
+
+    public void addStudentUser(String name, boolean feePaid){   
+        users.add(new Student(name, feePaid));
+    }
+
+    public void addFacultyMemberUser(String name, String department){   
+        users.add(new FacultyMember(name, department));
+    }
+
+    public Book findBookByTitle(String title){
+        return books.stream().filter(b -> b.getTitle().equals(title)).findFirst().orElse(null);
+    }
+
+    public User findUserByName(String name){    
+        return users.stream().filter(u -> u.getName().equals(name)).findFirst().orElse(null);
+    }
+
+    public void borrowBook(User user, Book book){
+        lendings.add(new Lending(book, user));
+    }
+
+    public void extendLending(FacultyMember facultyMember, Book book, LocalDate newDueDate){
+        lendings.stream().filter(l -> l.getBook().equals(book) && l.getUser().equals(facultyMember)).findFirst().ifPresent(l -> l.setDueDate(newDueDate));
+    }
+
+    public void returnBook(User user, Book book){
+        lendings.removeIf(l -> l.getBook().equals(book) && l.getUser().equals(user));
+    }
 }
+
