a08d390c5523e0836da825aa4077eec59cba85f0
Gudrunmaria <gudrunmaria@gmail.com>
2025-03-11 11:31:48 +0000

    bætti við UserOrBookDoesNotExistException og  exceptions í librarysystem

---
+++ src/main/java/is/hi/hbv202g/assignment8/UserOrBookDoesNotExistException.java
@@ -0,0 +1,9 @@
+package is.hi.hbv202g.assignment8;
+
+public class UserOrBookDoesNotExistException extends Exception {
+    public UserOrBookDoesNotExistException(String message) {
+        super(message);
+    }
+}
+
+
