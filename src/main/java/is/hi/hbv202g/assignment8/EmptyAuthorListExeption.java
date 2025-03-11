6b783d631f29a7e5568ce06876311ce12cc580ae
Your Name <youremail@example.com>
2025-03-11 11:21:03 +0000

    EmptyAuthorListException.java

---
+++ src/main/java/is/hi/hbv202g/assignment8/EmptyAuthorListExeption.java
@@ -0,0 +1,9 @@
+package is.hi.hbv202g.assignment8;
+
+public class EmptyAuthorListExeption extends Exception {
+
+    public EmptyAuthorListExeption(String message) {
+        super(message);
+    }
+    
+}
